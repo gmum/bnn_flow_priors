2a9017fe93149ebe2925aca69f6b88a594b75bc8
Bartosz Wojcik <bartwojc@gmail.com>
2022-08-13 17:47:03 +0200

    Add initial version of MFVI train script

---
+++ experiments/train_mfvi.py
@@ -0,0 +1,324 @@
+import json
+import logging
+import uuid
+import warnings
+from pathlib import Path
+
+import torch as t
+from torch.distributions import Normal
+from torch.nn.functional import softplus
+
+from bnn_priors import data as bnn_priors_data
+from bnn_priors import exp_utils
+from bnn_priors.exp_utils import get_model
+
+logging.basicConfig(
+    format=(
+        '[%(levelname)s:%(process)d %(module)s:%(lineno)d %(asctime)s] ' '%(message)s'
+    ),
+    level=logging.INFO,
+    handlers=[logging.StreamHandler()],
+    force=True,
+)
+logging.info('Configured logging.')
+
+if t.cuda.is_available():
+    t.backends.cudnn.benchmark = True
+
+TMPDIR = "/tmp"
+
+# TODO extract config to a different run file
+# config
+
+# the dataset to be trained on, e.g., "mnist", "cifar10", "UCI_boston"
+data = "mnist"
+# the inference method to be used, defaults to GGMC from https://arxiv.org/abs/2102.01691
+inference = "VerletSGLDReject"
+# model to be used, e.g., "classificationdensenet", "classificationconvnet", "googleresnet"
+model = "classificationconvnet"
+# width of the model (might not have an effect in some models)
+width = 50
+# depth of the model (might not have an effect in some models)
+depth = 3
+# weight prior, e.g., "gaussian", "laplace", "student-t"
+weight_prior = "gaussian"
+# bias prior, same as above
+bias_prior = "gaussian"
+# location parameter for the weight prior
+weight_loc = 0.
+# scale parameter for the weight prior
+weight_scale = 2. ** 0.5
+# location parameter for the bias prior
+bias_loc = 0.
+# scale parameter for the bias prior
+bias_scale = 1.
+# additional keyword arguments for the weight prior
+weight_prior_params = {}
+# additional keyword arguments for the bias prior
+bias_prior_params = {}
+if not isinstance(weight_prior_params, dict):
+    weight_prior_params = json.loads(weight_prior_params)
+if not isinstance(bias_prior_params, dict):
+    bias_prior_params = json.loads(bias_prior_params)
+# total number of samples to be drawn from the posterior
+n_samples = 300
+# number of learning rate cycles for the Markov chain (see https://arxiv.org/abs/1902.03932)
+cycles = 3  # 60
+# number of epochs per cycle without added Langevin noise
+burnin = 0
+# number of epochs per cycle with added noise, but without sampling
+warmup = 45
+# number of epochs to skip between taking samples (1 means no skipping)
+skip = 1
+# number of epochs to skip between computing metrics
+metrics_skip = 10
+# number of first samples to discard when evaluating them
+skip_first = 50  # for evaluating accuracy et al at the end
+# temperature of the sampler
+temperature = 1.0
+# learning rate schedule during sampling
+sampling_decay = "cosine"
+# momentum for the sampler
+momentum = 0.994
+# update factor for the preconditioner
+precond_update = 1
+# learning rate
+lr = 5e-4
+# initialization method for the network weights
+init_method = "he"
+# previous samples to be loaded to initialize the chain
+load_samples = None
+# batch size for the training
+batch_size = 128
+# whether to use Metropolis-Hastings rejection steps (works only with some integrators)
+reject_samples = False
+# whether to use batch normalization
+batchnorm = True
+# device to use, "cpu", "cuda:0", "try_cuda"
+device = "try_cuda"
+# whether the samples should be saved
+save_samples = True
+# whether a progressbar should be plotted to stdout during the training
+progressbar = True
+# a random unique ID for the run
+run_id = uuid.uuid4().hex
+
+assert inference in ["SGLD", "HMC", "VerletSGLD", "OurHMC", "HMCReject", "VerletSGLDReject", "SGLDReject"]
+assert width > 0
+assert n_samples > 0
+assert cycles > 0
+assert temperature >= 0
+
+
+# data & device
+def device(device: str):
+    if device == "try_cuda":
+        if t.cuda.is_available():
+            return t.device("cuda:0")
+        else:
+            return t.device("cpu")
+    return t.device(device)
+
+
+logging.info(f'Device: {device("try_cuda")}')
+dataset = bnn_priors_data.MNIST(device=device("try_cuda"), download=True)
+
+data = dataset
+x_train = data.norm.train_X
+y_train = data.norm.train_y
+
+x_test = data.norm.test_X
+y_test = data.norm.test_y
+
+# model
+model = get_model(x_train, y_train, model,
+                  width, depth,
+                  weight_prior, weight_loc, weight_scale,
+                  bias_prior, bias_loc, bias_scale,
+                  batchnorm, weight_prior_params, bias_prior_params)
+
+model.to(device("try_cuda"))
+
+# init
+if load_samples is None:
+    if init_method == "he":
+        exp_utils.he_initialize(model)
+    elif init_method == "he_uniform":
+        exp_utils.he_uniform_initialize(model)
+    elif init_method == "he_zerobias":
+        exp_utils.he_zerobias_initialize(model)
+    elif init_method == "prior":
+        pass
+    else:
+        raise ValueError(f"unknown init_method={init_method}")
+else:
+    state_dict = exp_utils.load_samples(load_samples, idx=-1, keep_steps=False)
+    model_sd = model.state_dict()
+    for k in state_dict.keys():
+        if k not in model_sd:
+            logging.warning(f"key {k} not in model, ignoring")
+            del state_dict[k]
+        elif model_sd[k].size() != state_dict[k].size():
+            logging.warning(f"key {k} size mismatch, model={model_sd[k].size()}, loaded={state_dict[k].size()}")
+            state_dict[k] = model_sd[k]
+
+    missing_keys = set(model_sd.keys()) - set(state_dict.keys())
+    logging.warning(f"The following keys were not found in loaded state dict: {missing_keys}")
+    model_sd.update(state_dict)
+    model.load_state_dict(model_sd)
+    del state_dict
+    del model_sd
+
+logging.info(f'{load_samples=}')
+logging.info(f'{init_method=}')
+
+# TODO what is _run? (seems to be a local argument from other functions)
+# if save_samples:
+#     model_saver_fn = (lambda: exp_utils.HDF5ModelSaver(
+#         exp_utils.sneaky_artifact(_run, "samples.pt"), "w"))
+# else:
+#     @contextlib.contextmanager
+#     def model_saver_fn():
+#         yield None
+
+# MFVI approx posterior:
+# locs   = {n: t.randn((p.shape), requires_grad=True, device=device(0)) for n, p in model.named_parameters() }
+locs = {n: t.tensor((p.cpu().detach().numpy()), requires_grad=True, device=device(0)) for n, p in
+        model.named_parameters()}
+scales = {n: t.randn((p.shape), requires_grad=True, device=device(0)) for n, p in model.named_parameters()}
+
+
+def sample_posterior(n_samples, only_pointwise_locs=False):
+    qs = {n: Normal(l, softplus(s) + 1e-8) for (n, l), s in zip(locs.items(), scales.values())}
+
+    if only_pointwise_locs:  # for point-wise training simply return repeated locs as samples:
+        samples = {n: l.repeat(t.Size([n_samples] + [1 for _ in l.shape])) for n, l in locs.items()}
+    else:  # for reparametrized gradients:
+        samples = {name: q.rsample(t.Size([n_samples])) for name, q in qs.items()}
+
+    return qs, samples
+
+
+approximation_params = list(locs.values()) + list(scales.values())
+
+
+def overwrite_params(module, samples, path="", skip_biases=False):
+    for name, m in module._modules.items():
+        overwrite_params(m, samples, path=path + "." + name, skip_biases=skip_biases)
+
+    for name, p in module._parameters.items():
+        if skip_biases and "bias" in name: continue
+        sample_path = (path + "." + name)[1:]  # skip the leading dot
+        new_value = samples[sample_path]
+        # assert new_value.shape == module._parameters[name].shape, f"sample_path={sample_path} shape={new_value.shape} current shape={module._parameters[name].shape}"
+        module._parameters[name] = new_value
+
+    return module
+
+
+# iterate over samples (taken from the original code)
+
+def _n_samples_dict(samples):
+    n_samples = min(len(v) for _, v in samples.items())
+
+    if not all((len(v) == n_samples) for _, v in samples.items()):
+        warnings.warn("Not all samples have the same length. "
+                      "Setting n_samples to the minimum.")
+    return n_samples
+
+
+def sample_iter(samples):
+    for i in range(_n_samples_dict(samples)):
+        yield dict((k, v[i]) for k, v in samples.items())
+
+
+NEPOCHS = 1000
+NSAMPLES_LEARNING = 1
+NSAMPLES_TESTING = 100
+LR = 0.001
+
+num_workers = (0 if isinstance(data.norm.train, t.utils.data.TensorDataset) else 2)
+dataloader = t.utils.data.DataLoader(data.norm.train, batch_size=batch_size, shuffle=True, drop_last=False,
+                                     num_workers=num_workers)
+dataloader_test = t.utils.data.DataLoader(data.norm.test, batch_size=batch_size, shuffle=False, drop_last=False,
+                                          num_workers=num_workers)
+
+
+def evaluate_model(model, dataloader_test, samples):
+    return exp_utils.evaluate_model(
+        model=model, dataloader_test=dataloader_test, samples=samples,
+        likelihood_eval=True, accuracy_eval=True,
+        calibration_eval=False)
+
+
+# evaluate before training
+# sample_epochs = n_samples * skip // cycles
+# n_samples = sample_epochs*cycles-skip_first
+n_samples = NSAMPLES_TESTING
+logging.info(f"before-training evaluation using n_samples={n_samples}")
+
+qs, posterior_samples = sample_posterior(n_samples)
+prior_samples = {name: buffer.repeat(n_samples) for name, buffer in model.named_buffers()}
+
+samples = {**posterior_samples, **prior_samples}
+evaluate_model(model, dataloader_test, samples)
+
+optimized_parameters = approximation_params
+optimizer = t.optim.Adam(optimized_parameters, lr=LR)
+
+# train
+learn_distributions = True
+for e in range(NEPOCHS):
+    logging.info(f'Start epoch: {e}')
+
+    total_log_likelihood, total_log_prior, total_entropy = 0., 0., 0.
+    for x, y in dataloader:
+        optimizer.zero_grad()
+
+        # sampling from approximate posterior
+        qs, posterior_samples = sample_posterior(NSAMPLES_LEARNING, only_pointwise_locs=not learn_distributions)
+        prior_samples = {name: buffer.repeat(NSAMPLES_LEARNING) for name, buffer in model.named_buffers()}
+
+        log_likelihood, log_prior, entropy = 0., 0., 0.
+        for sample_i, sample in enumerate(sample_iter({**posterior_samples, **prior_samples})):
+            # model.load_state_dict(sample, strict=True)
+            overwrite_params(model, sample)
+
+            # likelihood:
+            # preds = model(x)
+            # log_likelihood += preds.log_prob(y).sum() * x_train.shape[0]/x.shape[0]
+            log_likelihood += model.log_likelihood(x, y, x_train.shape[0])  # ???
+
+            if learn_distributions:
+                # priors:
+                log_prior += model.log_prior()
+
+                # entropy:
+                # entropy += sum( q.entropy().sum() for q in qs.values() ) # closed-form for Gaussian
+                entropy += sum(
+                    -q.log_prob(s).sum() for q, s in zip(qs.values(), posterior_samples.values()))  # via samples
+
+        elbo = log_likelihood + log_prior + entropy
+        loss_vi = -elbo
+        loss_vi.backward()
+        optimizer.step()
+
+        total_log_likelihood += log_likelihood
+        total_log_prior += log_prior
+        total_entropy += entropy
+
+    total_loss_vi = -total_log_likelihood - total_log_prior - total_entropy
+    logging.info(
+        f"epoch={e} loss_vi={total_loss_vi: .0f} (lik:{total_log_likelihood:.0f} prior:{total_log_prior:.0f} entropy:{total_entropy:.0f})")
+    if e % 10 == 0 and e > 0:
+        qs, posterior_samples = sample_posterior(NSAMPLES_TESTING)
+        prior_samples = {name: buffer.repeat(NSAMPLES_TESTING) for name, buffer in model.named_buffers()}
+        logging.info(evaluate_model(model, dataloader_test, {**posterior_samples, **prior_samples}))
+
+# save model
+# TODO add unique run names?
+state = model.state_dict()
+run_dir = Path.cwd() / 'runs'
+state_path = run_dir / 'model.pth'
+t.save(state, state_path)
+logging.info(f'Saved state to local path {str(state_path)}')
